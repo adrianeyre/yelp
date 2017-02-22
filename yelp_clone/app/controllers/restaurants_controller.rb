1735bf60af8dc42e4b87af116247df26f1a0a095
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-02-22 17:02:12 +0000

    linked restaurants to users with foreign key

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -10,7 +10,8 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant = Restaurant.new(restaurant_params)
+    # @restaurant = Restaurant.new(restaurant_params)
+    @restaurant = Restaurant.new(name: restaurant_params["name"], description: restaurant_params["description"], user_id: current_user.id)
     if @restaurant.save
       redirect_to restaurants_path
     else
--- yelp_clone/app/models/restaurant.rb
+++ yelp_clone/app/models/restaurant.rb
@@ -4,5 +4,6 @@
 
 class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
+  has_many :users
   validates :name, length: { minimum: 3 }, uniqueness: true
 end
