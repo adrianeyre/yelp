6390aeccf82909e5fa8d3912af42f0666976eef3
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-02-23 16:52:47 +0000

    endorsments and tests working

--- yelp_clone/spec/features/endorsements_feature_spec.rb
+++ yelp_clone/spec/features/endorsements_feature_spec.rb
@@ -2,22 +2,15 @@ require 'rails_helper'
 
 feature 'endorsing reviews' do
   before do
-    kfc = Restaurant.create(name: 'KFC')
-    kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
-    kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
+    sign_up
+    create_restaurant
+    leave_review('So so', '3')
   end
 
-  scenario 'a user can endorse a review, which updates the review endorsement count' do
-    # visit '/restaurants'
-    # click_link 'Endorse Review'
-    # expect(page).to have_content('1 endorsement')
-    it 'a user can endorse a review, which increments the endorsement count', js: true do
-      visit '/restaurants'
-      click_link 'Endorse'
-      expect(page).to have_content("1 endorsement")
-    end
+  it 'a user can endorse a review, which increments the endorsement count', js: true do
+    visit '/restaurants'
+    click_link 'Endorse'
+    expect(page).to have_content("1 endorsement")
   end
-
-
 
 end
